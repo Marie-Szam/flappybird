1d3dabbf067a52f54ad4c734b752c23d72c2a231
Marie Szamborski <marie.szamborski@43375.auckland.natcoll.ac.nz>
2014-04-09 16:07:53 +1200

    kiwi time

--- load.js
+++ load.js
@@ -2,7 +2,7 @@
 var load_state = {  
     preload: function() { 
         this.game.stage.backgroundColor = '#71c5cf';
-        this.game.load.image('bird', 'assets/bird.png');  
+        this.game.load.image('bird', 'assets/flappykiwi.png');  
         this.game.load.image('pipe', 'assets/pipe.png');  
         this.game.load.audio('jump', 'assets/jump.wav');
     },
